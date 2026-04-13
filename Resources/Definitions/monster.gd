7f23766b61e8ea49439f340cdb012ce6c99459ff
jonahmakowski <jonah@makowski.ca>
2026-04-13 09:22:49 -0400

    Added a texture2d tothe monster
    For the city mode

--- Resources/Definitions/monster.gd
+++ Resources/Definitions/monster.gd
@@ -3,6 +3,7 @@ extends Resource
 
 @export var to_drop: Item
 @export var model: PackedScene
+@export var sprite: Texture2D
 @export var health: int
 @export var damage: float
 @export var speed: float
