070fcfaa08796c7f58e2a84f02e4b81908673761
jonahmakowski <jonah@makowski.ca>
2026-05-21 11:16:49 -0400

    Fixed some issues

--- Globals/event_bus.gd
+++ Globals/event_bus.gd
@@ -1,6 +1,9 @@
 @tool
 extends Node
 
+@warning_ignore("unused_signal")
 signal update_inventory
+@warning_ignore("unused_signal")
 signal run_summoners
+@warning_ignore("unused_signal")
 signal hide_building_ui
--- Scenes/City/Building/UpgradeUI/upgrade_ui.gd
+++ Scenes/City/Building/UpgradeUI/upgrade_ui.gd
@@ -22,14 +22,14 @@ func _ready() -> void:
 func update():
 	icon.texture = upgrade.icon
 	title.text = upgrade.name
+
 	for type in upgrade.price:
 		var cost = cost_marker_scene.instantiate()
 		cost_container.add_child(cost)
 		cost.set_text(str(upgrade.price[type]))
 		cost.set_texture(type.sprite)
 		cost.set_tooltip("%s x %d" % [type.name, upgrade.price[type]])
-		print(type.name)
-		print(upgrade.price[type])
+
 	description.text = upgrade.description
 
 	purchase_button.disabled = not upgrade.can_purchase()
