c632af7b8d07f291339edc20ae44b8b4ca2de7b7
jonahmakowski <jonah@makowski.ca>
2026-06-02 22:27:31 -0400

    Made double jump upgrades

--- Resources/Definitions/player_data.gd
+++ Resources/Definitions/player_data.gd
@@ -10,14 +10,14 @@ extends Resource
 @export var max_inventory_size: int = 5
 @export var max_energy: int = 60
 @export var city_max_health: int = 20
-@export var city_repair_speed: int = 5
+@export var city_repair_speed: int = 1
 @export var enemy_damage: int = 2
 @export var speed: float = 25.0
 @export var jump_velocity: float = 15.0
-@export var max_jumps: int = 2
+@export var max_jumps: int = 1
 @export var mine_speed: float = 0.5
 @export var mine_damage: int = 1
-@export var grapling_hook_range: int = 100
+@export var grapling_hook_range: int = 50
 @export var current_wave: int = 0
 @export_group("Enemy Stats")
 @export var enemy_speed_mult: float = 0.9
